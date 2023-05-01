ef3bb1ef43151918d8be4338209532478c459c01
Calvin Ludwig <bludwig.calvin@gmail.com>
2023-05-01 15:03:37 -0300

    feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import healthcheck from 'fastify-healthcheck'
+
+const fastify = Fastify({
+	logger: true,
+})
+
+fastify.register(healthcheck)
+
+fastify.get('/', async (request, reply) => {
+	reply.type('application/json').code(200)
+	return { hello: 'world' }
+})
+
+fastify.listen({ port: 3000 }, (err, address) => {
+	console.log(`!!! server listening on ${address}`)
+	if (err) throw err
+})
